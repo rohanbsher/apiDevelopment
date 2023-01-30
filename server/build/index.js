97249340d2eb7e74659982b4c72685d21259947d
Rohan Bhandari <bhandarirohan556@gmail.com>
2023-01-30 00:20:58 -0500

    adding server
    adding server

---
+++ server/build/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+//option use of types here 
+app.get('/', (req, res) => {
+    res.send(`
+		<div>
+			<h1>Hi there!</h1>
+		</div>
+	`);
+});
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
